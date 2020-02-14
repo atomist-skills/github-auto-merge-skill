cb5295a27d662ea511182d4703a2ca27e59c1a17
Christian Dupuis <cd@atomist.com>
2020-02-14 16:12:15 +0100

    Handle multiple returns

--- events/AutoMergeOnStatus.ts
+++ events/AutoMergeOnStatus.ts
@@ -24,9 +24,17 @@ import { AutoMergeOnStatusSubscription } from "./types";
 
 export const handler: EventHandler<AutoMergeOnStatusSubscription, AutoMergeConfiguration> = async ctx => {
     const prs = ctx.data.Status[0].commit.pullRequests;
+    const results = [];
     for (const pr of prs) {
         const { owner, name } = pr.repo;
         const credentials = await ctx.credential.resolve(gitHubAppToken({ owner, repo: name }));
-        return executeAutoMerge(pr, ctx.configuration?.parameters, credentials);
+        const result = await executeAutoMerge(pr, ctx.configuration?.parameters, credentials);
+        if (!!result) {
+            results.push(result);
+        }
     }
+    return {
+        code: results.filter(r => !!r.code).some(r => r.code !== 0) ? 1 : 0,
+        reason: results.filter(r => !!r.reason).join("\n"),
+    };
 };
