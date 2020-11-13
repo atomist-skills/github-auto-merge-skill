6d2d3e3a3a8a82fbac73e3555748c3451b6f973d
David Dooling <david@atomist.com>
2020-11-13 15:45:25 -0600

    Remove unused chat provider
    [changelog:removed]

--- skill.ts
+++ skill.ts
@@ -39,7 +39,6 @@ export const Skill = skill<AutoMergeConfiguration & { repos: any }>({
 
 	resourceProviders: {
 		github: resourceProvider.gitHub({ minRequired: 1 }),
-		slack: resourceProvider.chat({ minRequired: 0 }),
 	},
 
 	parameters: {
